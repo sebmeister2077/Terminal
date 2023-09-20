29bee92cb3dde6ccd8adccceee03315a7b4a6434
romanunr02 <romanunr02@gmail.com>
2023-09-20 19:54:24 +0300

    initialize astro project

--- src/app/App.tsx
+++
@@ -1,8 +0,0 @@
-import { BrowserRouter, useLocation } from 'react-router-dom';
-import { Terminal } from '../pages/Terminal';
-
-export const App = () => {
-    const location = useLocation();
-    if (location.pathname == '/blank') return <></>;
-    return <Terminal />;
-};
--- src/shared/helpers/stringManipulators.ts
+++
@@ -1,2 +0,0 @@
-export const getTextWithRemovedIndex = (text: string, idx: number) => text.slice(0, idx - 1) + text.slice(idx);
-export const addTextWithAtIndex = (text: string, idx: number, addedText: string) => text.slice(0, idx) + addedText + text.slice(idx);
