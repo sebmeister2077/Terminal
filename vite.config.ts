440237632c80782894287dafce453b43294f0736
romanunr02 <romanunr02@gmail.com>
2023-09-24 16:28:17 +0300

    should work

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                app: '/public/index.html',
-            },
-        },
-    },
     plugins: [react()],
 });
